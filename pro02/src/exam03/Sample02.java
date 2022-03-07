65af7ce0adde5fa0a4d4345ba48918a94eb7271d
donggri <donggri@DESKTOP-JNFN7D9>
2022-03-07 15:16:27 +0900

    커밋

---
+++ pro02/src/exam03/Sample02.java
@@ -0,0 +1,32 @@
+package exam03;
+
+import java.util.Scanner;
+
+public class Sample02 {
+
+	public static void main(String[] args) {
+		/*
+		 * 사용자 입력을 받기 위한 Scanner 클래스
+		 * 		- 기본적으로 모든 사용자 입력 데이터는 문자열이다.
+		 * 		- 정수 또는 실수 데이터를 사용하기 위해서 별도의 함수를 사용해야 한다. -> sc.nextInt(), sc.nextDouble()
+		 */
+
+		// Scanner 객체 생성
+		Scanner sc = new Scanner(System.in);
+		
+		// 사용자 입력 받기. (정수만 입력 받도록 되어 있음)
+		System.out.print("정수를 입력하세요 : ");
+		int iInput = sc.nextInt();
+		
+		System.out.println("사용자 입력 값 : " + iInput);
+		System.out.printf("사용자 입력 값 %d\n", iInput);
+		
+		// 사용자 입력 받기. (실수만 입력 받도록 되어 있음)
+		System.out.print("실수를 입력하세요 : ");
+		double dInput = sc.nextDouble();
+			
+		System.out.println("사용자 입력 값 : " + dInput);
+		System.out.printf("사용자 입력 값 %f\n", dInput);
+	}
+
+}
