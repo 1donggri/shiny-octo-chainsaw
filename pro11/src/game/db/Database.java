ffe2d3edec1d10be91ff5da1c0c245e626673fbc
donggri <donggri@DESKTOP-JNFN7D9>
2022-04-15 09:11:43 +0900

    4월 14일 커밋

--- pro11/src/game/db/Database.java
+++ pro11/src/game/db/Database.java
@@ -1,10 +1,21 @@
 package game.db;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+import game.record.Record;
 
 public class Database {
 	
 	private File file;
+	private Record rec;
 	
 	public Database() {
 		this.file = new File("C:/Users/donggri/gbo_game.record");
@@ -16,11 +27,31 @@ public class Database {
 	
 	public int[] load() {
 		// 데이터 불러오기
-		return null;
+		int[] score = new int[3];
+		try(DataInputStream dis = new DataInputStream(new FileInputStream(file))){
+			for(int i = 0; i < score.length; i++) {
+				score[i] = dis.readInt();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return score;
 	}
 	
 	public void save(int[] record) {
 		// 데이터 저장하기
+		try(DataOutputStream dos = new DataOutputStream(new FileOutputStream(file))){
+			for(int i = 0; i < record.length; i++) {
+				dos.writeInt(record[i]);
+			}
+			dos.flush();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 
