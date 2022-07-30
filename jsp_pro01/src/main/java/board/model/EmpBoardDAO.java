1dbaa50142b6321ee8f51e98f2a2bafa0bdffaeb
donggri <donggri@DESKTOP-JNFN7D9>
2022-07-30 23:33:02 +0900

    #6
    JSP/Servlet 으로 변환함.

---
+++ jsp_pro01/src/main/java/board/model/EmpBoardDAO.java
@@ -0,0 +1,31 @@
+package board.model;
+import java.util.*;
+
+import org.apache.ibatis.session.SqlSession;
+
+/**
+ * 
+ */
+public class EmpBoardDAO {
+
+    /**
+     * Default constructor
+     */
+    public EmpBoardDAO() {
+    }
+
+    /**
+     * 
+     */
+    private SqlSession session;
+
+    /**
+     * @param data 
+     * @return
+     */
+    public boolean insertData(EmpBoardDTO data) {
+        // TODO implement here
+        return false;
+    }
+
+}
